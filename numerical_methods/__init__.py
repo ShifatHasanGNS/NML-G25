9fdf3523e13b5097e7ea869118c55eca01d62d0a
Md. Shifat Hasan <shifathasangns@gmail.com>
2024-10-25 19:35:00 +0600

    Edited __init__.py

--- numerical_methods/__init__.py
+++ numerical_methods/__init__.py
@@ -1,6 +1,6 @@
 from .utils import *
 
 # Asif Jawad
-from Asif_Jawad.bi_section import solve_using_bisection_method
-from Asif_Jawad.false_position import solve_using_false_position_method
-from Asif_Jawad.lu_factorization import solve_using_lu_factorization
+from Asif_Jawad_2107007.bi_section import solve_using_bisection_method
+from Asif_Jawad_2107007.false_position import solve_using_false_position_method
+from Asif_Jawad_2107007.lu_factorization import solve_using_lu_factorization
